5a924ad09104c2d70e644a467d9ca27906d71830
chanhpark <chanhyun.park1211@gmail.com>
2015-02-21 10:31:24 -0500

    css tic-tac-toe switching bewteen players

---
+++ js/tic_tac_toe.js
@@ -0,0 +1,15 @@
+// Initialize current player as player 'x'
+var currentPlayerToken = 'x';
+
+$('.board').on('click', ".square:not('.square-x, .square-o')", function(event) {
+  // Select the square
+  var $square = $(event.currentTarget);
+  $square.addClass('square-' + currentPlayerToken);
+
+  // Swap current player's token
+  if (currentPlayerToken === 'x') {
+    currentPlayerToken = 'o';
+  } else {
+    currentPlayerToken = 'x';
+  }
+});
