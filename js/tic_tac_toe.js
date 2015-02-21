b4e50b96a36c6a565b5b9a0075563e326b09a185
chanhpark <chanhyun.park1211@gmail.com>
2015-02-21 10:37:28 -0500

    check choices for winners

--- js/tic_tac_toe.js
+++ js/tic_tac_toe.js
@@ -17,19 +17,6 @@ var winningCombinations = [
 // Initialize current player as player 'x'
 var currentPlayerToken = 'x';
 
-$('.board').on('click', ".square:not('.square-x, .square-o')", function(event) {
-  // Select the square
-  var $square = $(event.currentTarget);
-  $square.addClass('square-' + currentPlayerToken);
-
-  // Swap current player's token
-  if (currentPlayerToken === 'x') {
-    currentPlayerToken = 'o';
-  } else {
-    currentPlayerToken = 'x';
-  }
-});
-
 // Initialize a hash to store all of the chosen squares for each player
 var chosenSquares = {
   'x': [],
@@ -45,7 +32,27 @@ $('.board').on('click', ".square:not('.square-x, .square-o')", function(event) {
   var indexOfSquare = $('.board .square').index($square);
   var currentPlayerSquares = chosenSquares[currentPlayerToken]
   currentPlayerSquares.push(indexOfSquare);
-  console.log(chosenSquares);
+
+  // Check for win
+  //
+  // For each winning combination
+  $.each(winningCombinations, function(index, combination) {
+    // Stary by assuming that the player has all of the squares
+    var hasAllSquares = true;
+
+    // For each of squares in the combination
+    $.each(combination, function(index, square) {
+      // If the player's chosen squares does not contain the current square
+      if ($.inArray(square, currentPlayerSquares) === -1) {
+        hasAllSquares = false;
+      }
+    });
+
+    // Display the winner
+    if (hasAllSquares) {
+      alert(currentPlayerToken + ' wins!');
+    }
+  });
 
   // Swap current player's token
   if (currentPlayerToken === 'x') {
